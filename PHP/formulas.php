60e18466315ffaae700c8de156794dd9c6a6b87b
Evan Mitchell <114937545+mitch172@users.noreply.github.com>
2023-06-20 09:36:18 -0400

    Update formulas.php

--- PHP/formulas.php
+++ PHP/formulas.php
@@ -14,10 +14,10 @@
 	// If input is a new hire
 	if($class == "NH") {
 		// Initialize account and variables
-		$start    = $_POST['start_date']; 
-		$name     = $_POST['full_name']; 
+		$start = $_POST['start_date']; 
+		$name = $_POST['full_name']; 
 		$nickname = $_POST['nickname']; 
-		$title    = $_POST['title'];
+		$title = $_POST['title'];
 		$location = $_POST['location'];
 		$computer = $_POST['computer'];
 		$status = "";
@@ -27,33 +27,34 @@
 		$qc = "";
 
 		//Insert into newHires table
-		$sql = "INSERT INTO newHires
-			(start,name,nickname,title,location,computer,status,serial,asset,tech,qc) VALUES
+		$sql = "INSERT INTO newHires (start,name,nickname,title,location,computer,status,serial,asset,tech,qc) VALUES
 			('$start','$name','$nickname','$title','$location','$computer','$status','$serial','$asset','$tech','$qc')";
 		if (mysqli_query($conn, $sql)) {
 			$url = "http://withumbuildproject.com/HTML/newHire.html";
-			echo "<script>alert('New record has been added successfully!')</script>
-				<meta http-equiv='Refresh' content='0; url=$url' />";
+			echo "<script>alert('New record has been added successfully!')</script><meta http-equiv='Refresh' content='0; url=$url' />";
 		} else {
 			echo "Error: " . $sql . ":-" . mysqli_error($conn);
 		}
 	// If input is an upgrade or rebuild
 	} else {
 		// Initialize account and variables
-		$name    	= $_POST['full_name'];
-		$location 	= $_POST['location'];
+		$name = $_POST['full_name'];
+		$location = $_POST['location'];
 		$build_location = "RB";
-		$computer 	= $_POST['computer'];
-		$windows 	= $_POST['windows'];
+		$computer = $_POST['computer'];
+		$windows = $_POST['windows'];
+		$status = "";
+		$serial = "";
+		$asset = "";
+		$tech = "";
+		$qc = "";
 
 		//Insert into upgrades table
-		$sql = "INSERT INTO upgrades
-			(name,location,buildLocation,computer,windows) VALUES
+		$sql = "INSERT INTO upgrades (name,location,buildLocation,computer,windows) VALUES
 			('$name','$location','$build_location','$computer','$windows')";
 		if (mysqli_query($conn, $sql)) {
 			$url = "http://withumbuildproject.com/HTML/upgrades.html";
-			echo "<script>alert('New record has been added successfully!')</script>
-				<meta http-equiv='Refresh' content='0; url=$url' />";
+			echo "<script>alert('New record has been added successfully!')</script><meta http-equiv='Refresh' content='0; url=$url' />";
 		} else {
 			echo "Error: " . $sql . ":-" . mysqli_error($conn);
 		}
