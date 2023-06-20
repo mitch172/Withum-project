4a573d11bf3c1668df76d404330696138bb64998
Evan Mitchell <114937545+mitch172@users.noreply.github.com>
2023-06-20 14:50:58 -0400

    Update builder.php

--- PHP/builder.php
+++ PHP/builder.php
@@ -1,3 +1,56 @@
-<?php
+<html>
+  <head>
+    <title>Builder's Work</title>
+  </head>
 
-?>
+  <body>
+    <table width="75%">
+      <tr>
+        <td width="33%">Name</td>
+        <td width="33%">Tech</td>
+        <td width="33%">QC</td>
+      </tr>
+      <?php
+        $builder = $_POST['name'];
+      
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "withum";
+      
+        $connection = new mysqli($servername,$username,$password,$database);
+        if($connection->connect_error) {
+          die("Connection failed: " . $connection->connect_error);
+        }
+      
+        $sql = "SELECT * from newHires ORDER BY start ASC WHERE tech='$builder' OR qc='$builder'";
+        $result = $connection->query($sql);
+        if(!$result) {
+          die("Invalid query: " . $connection->error);
+        }
+      
+        while($row = $result->fetch_assoc()) {
+          echp "<tr>
+            <td>" . $row['name'] . "</td>
+            <td>" . $row['tech'] . "</td>
+            <td>" . $row['qc'] . "</td>
+          </td>";
+        }
+        
+        $sql = "SELECT * from upgrades ORDER BY start ASC WHERE tech='$builder' OR qc='$builder'";
+        $result = $connection->query($sql);
+        if(!$result) {
+          die("Invalid query: " . $connection->error);
+        }
+      
+        while($row = $result->fetch_assoc()) {
+          echp "<tr>
+            <td>" . $row['name'] . "</td>
+            <td>" . $row['tech'] . "</td>
+            <td>" . $row['qc'] . "</td>
+          </td>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
